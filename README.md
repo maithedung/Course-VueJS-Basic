83ecdd9e994aee2e7805fb86f8b97d85f4813c1c
maithedung <maithedung.js@gmail.com>
2019-08-20 16:03:21 +0700

    05 - Data binding & JavaScript Expression

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1. Install VueJS
 2. VueInstance 
 3. Data & Method 
+4. Data binding
+5. JavaScript Expression
--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const vueInstance = new Vue({
   el: "#app",
   data: {
-    title: "Coders.Tokyo"
+    title: "Coders.Tokyo",
+    url: 'https://www.facebook.com/maithedung.nodejs',
+    target: '_blink',
+    price: 1000,
+    check: 20 > 1000
   },
   methods: {
-      say = function(text) {
-        return `Hello ${text}`
-      }
+    say(text) {
+      return `Hello ${text}`;
+    },
+    formatPrice() {
+      return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(this.price)
+    }
   }
 });
 // He thong phan ung Reactiriry
