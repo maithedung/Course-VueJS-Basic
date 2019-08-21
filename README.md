6f1f2ecb5881a7a51ae50701118368087c54f02e
maithedung <maithedung.js@gmail.com>
2019-08-21 14:31:53 +0700

    08-Tim hieu ve Computed

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 3. Data & Method 
 4. Data binding
 5. JavaScript Expression
+6. Event Handling
+    - click
+    - mouseMove
+    - submit
+    - .prevent (Ngan chan) || event.preventDeaufaul()
+    - event.target
+    - .stop || event.stopPropagation() (Dung su lan tu con ra cha)
+    - keyup
+7. Computed
--- main.js
+++ main.js
@@ -2,17 +2,66 @@ const vueInstance = new Vue({
   el: "#app",
   data: {
     title: "Coders.Tokyo",
-    url: 'https://www.facebook.com/maithedung.nodejs',
-    target: '_blink',
+    url: "https://www.facebook.com/maithedung.nodejs",
+    target: "_blink",
     price: 1000,
-    check: 20 > 1000
+    check: 20 > 1000,
+    counter: 0,
+    clientX: 0,
+    clientY: 0,
+    message: "Hello World",
+    a: 0,
+    b: 0,
+    number: 20
   },
   methods: {
     say(text) {
       return `Hello ${text}`;
     },
     formatPrice() {
-      return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(this.price)
+      return new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "VND"
+      }).format(this.price);
+    },
+    handleClick(event, number) {
+      // console.log("click", event);
+      this.counter += number;
+    },
+    handleMouseMove(event) {
+      // console.log("move", event);
+      this.clientX = event.clientX;
+      this.clientY = event.clientY;
+      console.log(this.clientX, this.clientY);
+    },
+    handleSubmitForm(event) {
+      console.log(event);
+      // event.preventDefault();
+    },
+    handleMouseMoveChild(event) {
+      console.log("HandleMouseMoveChild", event.target);
+      // event.stopPropagation();
+    }
+  },
+  computed: {
+    // a computed getter
+    reversedMessage() {
+      // `this` points to the vm instance
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
+    },
+    addA() {
+      console.log("Add A run");
+      return this.a + this.number;
+    },
+    addB() {
+      console.log("Add B run");
+      return this.b + this.number;
+    },
+    now() {
+      return Date.now()
     }
   }
 });
@@ -22,3 +71,7 @@ console.log(vueInstance);
 setTimeout(() => {
   vueInstance.title = "CodersX";
 }, 3000);
+
+console.log(vueInstance.reversedMessage);
+// vueInstance.message = "Say Goodbye";
+console.log(vueInstance.reversedMessage);
